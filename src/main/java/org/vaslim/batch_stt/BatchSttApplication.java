40f2d4d2a9859a225b016e549b6e692329587a06
vaslim <mislav.jancic@protonmail.com>
2023-09-01 20:49:42 +0200

    Initial commit

---
+++ src/main/java/org/vaslim/batch_stt/BatchSttApplication.java
@@ -0,0 +1,13 @@
+package org.vaslim.batch_stt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchSttApplication.class, args);
+	}
+
+}
