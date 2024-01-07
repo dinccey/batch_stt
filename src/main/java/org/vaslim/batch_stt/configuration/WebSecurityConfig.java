715998497a7917a4c250aeae8519914fa7a43daa
vaslim <mislav.jancic@protonmail.com>
2024-01-07 20:53:47 +0100

    Re-enable cors

--- src/main/java/org/vaslim/batch_stt/configuration/WebSecurityConfig.java
+++ src/main/java/org/vaslim/batch_stt/configuration/WebSecurityConfig.java
@@ -58,7 +58,6 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
